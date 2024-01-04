69b6500899a8815e397405c5804e54c83fa137a6
Luke Garnsey <lukegarnsey@gmail.com>
2024-01-03 19:49:40 -0500

    Added Storage for scores and dynamic displaying on high score screen.

--- assets/javascript/high-scores-script.js
+++ assets/javascript/high-scores-script.js
@@ -1,6 +1,26 @@
+const highScoreList = document.getElementById("high-score-list");
+
 document.getElementById("back").onclick = function(){
     window.location.href = "../../index.html";
 }
 document.getElementById("clear").onclick = function(){
-    console.log("CLEAR");
+    localStorage.clear();
+    while(highScoreList.children.length > 0){
+        highScoreList.removeChild(highScoreList.children[0]);
+    }
 }
+
+function init(){
+    var scoresObj = localStorage.getItem("scores");
+    if(scoresObj === null)
+        return;
+
+    scoresObj = JSON.parse(scoresObj);
+
+    scoresObj.scores.forEach(userScore => {
+        var listItem = document.createElement("li");
+        listItem.innerText = userScore.name + " - " + userScore.score;
+        highScoreList.append(listItem);
+    });
+}
+init();
--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -13,32 +13,46 @@ wrongElement.style.display = "none";
 correctElement.style.display = "none";
 
 var quizTimerInterval;
+var currentQuestion = 1;
+var finalScore = 0;
 
 function startTimer(startTime){
     timer.innerHTML = startTime;
     quizTimerInterval = setInterval(countdown, 1000);
 }
 function countdown(){
-    let time = parseInt(timer.innerHTML);
-    if(time == 0){
+    finalScore = parseInt(timer.innerHTML);
+    if(finalScore == 0){
+        document.getElementById("score").innerText = finalScore;
         clearInterval(quizTimerInterval);
         finishedElement.style.display = "block";
         quizQuestionElement.style.display = "none";
         return;
     }
-    time--;
-    timer.innerHTML = time;
+    finalScore--;
+    timer.innerHTML = finalScore;
     
 }
 document.getElementById("view-high-score").onclick = loadHighScores
 startCodingElement.getElementsByTagName("button")[0].onclick = function(){
-    startTimer(5);
+    startTimer(1);
     startCodingElement.style.display = "none";
     quizQuestionElement.style.display = "block";
 }
 
 function submitHighScore(){
+    // preventDefault();
+    const name = document.getElementById("fname").value;
+    var scoresObj = localStorage.getItem("scores");
+    console.log(scoresObj);
+    if(scoresObj === null){
+        scoresObj = {};
+        scoresObj.scores = [];
+    }else
+        scoresObj = JSON.parse(scoresObj);
 
+    scoresObj.scores.push({"name":name, "score":finalScore})
+    localStorage.setItem("scores", JSON.stringify(scoresObj));
     loadHighScores();
 }
 function loadHighScores(){
